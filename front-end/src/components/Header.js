c1469d34925ae1eae2b6da1751a7cdcbe99cb1c5
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-12-31 12:12:30 -0600

    created basic header.js component for the nav bar that will be used across pages

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,27 @@
+
+
+export default function Header(){
+    
+    return (
+        <header>
+            <h1>PROPHET JOSH</h1>
+            <nav>
+                <ul>
+                    <li>
+                        ABOUT
+                    </li>
+                    <li>
+                        TEACHING
+                    </li>
+                    <li>
+                        EVENTS
+                    </li>
+                    <li>
+                        DONATE
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        
+    )
+}
